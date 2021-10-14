7ad76efc7799690c27445bfc7ecc6f53f328c0ea
ncberman <ncberman@asu.edu>
2021-10-13 21:36:54 -0700

    Update DHTClient.java
    Detailed Client command descriptions

--- Client/DHTClient.java
+++ Client/DHTClient.java
@@ -20,6 +20,16 @@ public class DHTClient
             while(!command.equalsIgnoreCase("exit"))
             {
                 String[] tokenizedCommand = command.split(" ");
+                /*
+                    Our first command will be the 'register' command, this command requires 3 inputs variables being
+                        1. Username
+                        2. IP Address
+                        3. Port Number
+                    The command will construct a #-Separated string with a header using a default register command header 'RegisterUser'
+
+                    The purpose of this command is to tell the watch-server to register a new user with the given specifications. The server will
+                    ping the given user to confirm the registration and availability of newly added users.
+                */
                 if(tokenizedCommand[0].equalsIgnoreCase("register"))
                 {
                     String username = tokenizedCommand[1];
@@ -28,19 +38,62 @@ public class DHTClient
 
                     String registerMsg = "RegisterUser#" + username + "#" + ipv4 + "#" + port;
                 }
+                /*
+                    Our second command will be the 'setup-dht' command, takes 2 inputs variables being
+                        1. DHT-Size >= 2
+                        2. Username of DHT Leader
+                    This command will construct a #-Separated with the header using the default setup-dht header 'SetupDHT'
+
+                    The purpose of this command is to tell the watch-server to setup a distributed hash-table using the specified user
+                    as a leader and grabbing n-1 other users to construct the table framework.
+                */
                 else if(tokenizedCommand[0].equalsIgnoreCase("setup-dht"))
                 {
 
                 }
+                /*
+                    Our third command will be the 'dht-complete' command, takes 1 input variable being
+                        1. Username of DHT Leader
+                    This command will construct a #-Separated with the header using the default dht-complete header 'CompleteDHT'
+
+                    The purpose of this command is to see if the given user is the leader of the current DHT. The server will respond to this 
+                    message with either SUCCESS or FAILURE depending on if the state of the given username is 'LeaderDHT' or not. In relation
+                    a server will only recognize and set the state of a registered user to leader / worker if and only if the DHT has completed
+                    its setup.
+                */
                 else if(tokenizedCommand[0].equalsIgnoreCase("dht-complete"))
                 {
 
                 }
+                /*
+                    Our fourth command will be the 'query-dht' command, takes 1 input variable being
+                        1. Querying username
+                    This command will construct a #-Separated with the header using the default query-dht header 'QueryDHT'
+
+                    The purpose of this command is to see return the username, ip address, and port number of one of the various members of
+                    the DHT. If the username specified is a member of the DHT or is not registered with the server then the query fails.
+                */
                 else if(tokenizedCommand[0].equalsIgnoreCase("query-dht"))
                 {
 
                 }
+                else if(tokenizedCommand[0].equalsIgnoreCase("leave-dht"))
+                {
+
+                }
+                else if(tokenizedCommand[0].equalsIgnoreCase("dht-rebuilt"))
+                {
+
+                }
                 else if(tokenizedCommand[0].equalsIgnoreCase("deregister"))
+                {
+
+                }
+                else if(tokenizedCommand[0].equalsIgnoreCase("teardown-dht"))
+                {
+
+                }
+                else if(tokenizedCommand[0].equalsIgnoreCase("teardown-complete"))
                 {
 
                 }
